ed181e29eb302a2823412194bce55a278d819880
Akshat Arora <akshatarora826@gmail.com>
2017-06-13 19:33:12 +0530

    This folder contains the assets (html,css and java script)files for rendering the app

---
+++ public/assets/todo-list.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+			alert("Congratulations! You just added "+item.val()+" to your database");
+          
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+			alert("Your have successfully deleted the selected item from your database")
+       
+          location.reload();
+        }
+      });
+  });
+
+});
